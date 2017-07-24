2295175fe6ffa78a7ff0262ac40db5f020c430a4
wchang28 <wchang28@hotmail.com>
2017-07-23 21:45:21 -0400

    added spawn params

--- ts/services/index.ts
+++ ts/services/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as core from 'express-serve-static-core';
 import {IRequestData, JSONEndware} from "../request-data";
 import {StateMachineJSON, State} from "../state-machine";
+import {SpawnParams} from "../server-mgr";
 import {Router as serverRouter} from "./server";
 
 let router = express.Router();
@@ -12,4 +13,5 @@ router.get("/", JSONEndware<StateMachineJSON>((rqd: IRequestData) => Promise.res
 router.get("/state", JSONEndware<State>((rqd: IRequestData) => Promise.resolve<State>(rqd.StateMachine.State)));
 router.get("/deploy", JSONEndware<any>((rqd: IRequestData) => rqd.StateMachine.deploy()));
 router.get("/reload", JSONEndware<any>((rqd: IRequestData) => rqd.StateMachine.deploy()));
+router.get("/spawn-params", JSONEndware<SpawnParams>((rqd: IRequestData) => rqd.SpawnParamsStore.load()));
 router.use("/server", serverRouter);
